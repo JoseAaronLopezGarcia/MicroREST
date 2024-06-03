8f4081333dc5c4471c1ffde99322f4fba2f9b6bb
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2024-06-03 16:47:39 +0200

    cleanup

--- kotlin/org/MicroREST/ServerViewModel.kt
+++ kotlin/org/MicroREST/ServerViewModel.kt
@@ -2,9 +2,6 @@ package org.MicroREST
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import es.wenea.tpvapp.business.NativeApiEndpoint
-import es.wenea.tpvapp.business.NativeServer
-import es.wenea.tpvapp.domain.Configuration
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,12 +9,11 @@ class ServerViewModel @Inject constructor(): ViewModel() {
 
     init {
         System.loadLibrary("NativeServer");
-        NativeServer.setAuthorizationToken(Configuration.NATIVE_SERVER_TOKEN)
         NativeServer.enableTest()
     }
 
     fun start(){
-        NativeServer.startServer(Configuration.NATIVE_SERVER_PORT)
+        NativeServer.startServer(8080)
     }
 
     fun addApiEndpoints(api: List<NativeApiEndpoint>){
